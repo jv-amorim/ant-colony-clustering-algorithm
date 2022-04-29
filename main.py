a06b274ea7bc8940a0c41068843af328cf3a2164
Filip3ra <filipimacielrj@gmail.com>
2022-04-29 19:41:33 -0300

    problema python não encontrado, sem sucesso

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-import numpy as np
+#import numpy as np
 import random
 
 # matriz com 30 individuos com 9 elementos, preenchida com 0
-objetos = np.zeros((100, 9), dtype=int)
+#objetos = np.zeros((100, 9), dtype=int)
 
 
 # Função que vai preencher a matriz de objetos com valores aleatórios.
 def gera_objetos():
     # percorro cada elemento e adiciono um valor aleatório entre 0 e 15
-    for x in range(len(objetos)):
-        for y in range(len(objetos[x])):
-            objetos[x][y] = random.randint(0, 15)
+ #   for x in range(len(objetos)):
+ #       for y in range(len(objetos[x])):
+  #          objetos[x][y] = random.randint(0, 15)
 
-    gera_objetos()
+    #gera_objetos()
     print("teste")
