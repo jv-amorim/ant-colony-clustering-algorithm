83beceb756fdfdb07f0cd1186ff845704566aee2
Filip3ra <filipimacielrj@gmail.com>
2022-05-02 08:56:55 -0300

    gerando objetos, mas casas decimais não funcionam ainda

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-#import numpy as np
+import numpy as np
 import random
 
-# matriz com 30 individuos com 9 elementos, preenchida com 0
-#objetos = np.zeros((100, 9), dtype=int)
+# matriz com 10 objetos e duas características, altura e média de gols
+objetos = np.zeros((10, 2), dtype=int)
 
 
-# Função que vai preencher a matriz de objetos com valores aleatórios.
+# Função que vai preencher a matriz de objetos com valores aleatórios
 def gera_objetos():
-    # percorro cada elemento e adiciono um valor aleatório entre 0 e 15
- #   for x in range(len(objetos)):
- #       for y in range(len(objetos[x])):
-  #          objetos[x][y] = random.randint(0, 15)
+    # percorro cada elemento e adiciono um valor aleatório entre 0 e 1
+    for x in range(len(objetos)):
+        for y in range(len(objetos[x])):
+            if y == 0:
+                objetos[x][y] = round(random.uniform(1.5, 2.10), 2)
+                print("altura = ", objetos[x][y])
+            else:
+                objetos[x][y] = round(random.uniform(0, 1), 2)
+                print("media = ", objetos[x][y])
 
-    #gera_objetos()
-    print("teste")
+
+gera_objetos()
