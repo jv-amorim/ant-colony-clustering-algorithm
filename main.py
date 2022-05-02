8eb77dfd4c302aa7e8d9ee66111ae50ee1b4c782
Filip3ra <filipimacielrj@gmail.com>
2022-05-02 09:22:21 -0300

    outra abordagem para gerar objetos, funciona parcialmente

--- main.py
+++ main.py
@@ -4,19 +4,14 @@ import random
 # matriz com 10 objetos e duas características, altura e média de gols
 objetos = np.zeros((10, 2), dtype=int)
 
+# gera valores de média de gols
+media_gols = np.random.random((10, 1))
+print(media_gols)
 
-# Função que vai preencher a matriz de objetos com valores aleatórios
-def gera_objetos():
-    # percorro cada elemento e adiciono um valor aleatório entre 0 e 1
-    for x in range(len(objetos)):
-        for y in range(len(objetos[x])):
-            if y == 0:
-                objetos[x][y] = random.uniform(1.5, 2.10)
-                print("altura = ", "{:.2f}".format(objetos[x][y]))
-            else:
-                objetos[x][y] = random.uniform(0, 1)
-                print("media = ", "{:.2f}".format(objetos[x][y]))
+# gera valores para altura dos jogadores
+altura = [0] * 10
+for i in range(10):
+    altura[i] = np.random.uniform(1.5, 2.10)
 
-
-gera_objetos()
+print(altura)
 
