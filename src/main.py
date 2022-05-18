345f50983da40eac2c7728079add2d71a2aaae65
JV-Amorim <joaovitoramorimx@gmail.com>
2022-05-17 21:29:05 -0300

    O output do algoritmo agora é salvo em um arquivo-texto.

--- src/main.py
+++ src/main.py
@@ -14,8 +14,27 @@ def main():
   for _ in range(MAX_ITERATIONS):
     colony.perform_colony_iteration()
 
-  print('Result:')
-  print(grid_manager.grid + 1)
+  write_output_file(grid_manager)
+
+
+def write_output_file(grid_manager):
+  output_content = \
+    str(grid_manager.grid.tolist()) \
+    .replace('-1', '_') \
+    .replace('[', '') \
+    .replace('], ', '\n') \
+    .replace(']', '') \
+    .replace(',', '') \
+    .replace('_', '__')
+  
+  file = open('output/output.txt', 'w')
+
+  for item in output_content.split(' '):
+    if len(item) == 1:
+      item = '0' + item
+    file.write(item + ' ')
+
+  file.close()
 
 
 if __name__ == '__main__':
